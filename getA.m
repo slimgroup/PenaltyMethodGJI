7b296db3512635ed020d09fe260be170b008a4b2
tleeuwen <tristan.vanleeuwen@gmail.com>
2014-02-05 16:12:31 +0100

    Update getA.m
    fixed scaling issue in BC

--- getA.m
+++ getA.m
@@ -41,8 +41,8 @@ omega = 2*pi*f;
 N     = prod(n);
 
 % Stiffness matrices
-D1 = spdiags(ones(n(1),1)*[1 -2 1]/h(1).^2,[-1:1],n(1),n(1)); D1(1,1:2) = [1 -1]/h(1); D1(end,end-1:end) = [-1 1]/h(1);
-D2 = spdiags(ones(n(2),1)*[1 -2 1]/h(2).^2,[-1:1],n(2),n(2)); D2(1,1:2) = [1 -1]/h(2); D2(end,end-1:end) = [-1 1]/h(2);
+D1 = spdiags(ones(n(1),1)*[1 -2 1]/h(1).^2,[-1:1],n(1),n(1)); D1(1,1:2) = [1 -1]/h(1)^2; D1(end,end-1:end) = [-1 1]/h(1)^2;
+D2 = spdiags(ones(n(2),1)*[1 -2 1]/h(2).^2,[-1:1],n(2),n(2)); D2(1,1:2) = [1 -1]/h(2)^2; D2(end,end-1:end) = [-1 1]/h(2)^2;
 
 S = kron(speye(n(2)),D1) + kron(D2,speye(n(1)));
 
@@ -51,7 +51,10 @@ w = [0 ones(1,n(1)-2) 0];
 w = w(:)*[0 ones(1,n(2)-2) 0];
 w = w(:);
 
-M = omega^2*spdiags(w.*m,0,N,N) + 1i*omega*spdiags((1-w).*sqrt(m),0,N,N);
+v = [1 zeros(1,n(1)-2) 1]/h(1);
+v = v(:)*[1 zeros(1,n(1)-2) 1]/h(2);
+
+M = omega^2*spdiags(w.*m,0,N,N) + 1i*omega*spdiags(v.*sqrt(m),0,N,N);
 
 % 
 A = M + S;
